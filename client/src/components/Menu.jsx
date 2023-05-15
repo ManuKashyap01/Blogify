f8b6625ce4b393f5b65094318c318aaeb1e02981
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-05-15 17:09:27 +0530

    some design updates

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Post=({post})=>{
     return (
         <div className="post" id={post.id}>
-            <img className='max-h-[200px] object-center w-full' src={post.img} alt="" />
+            <img className='max-h-[200px] object-center object-cover w-full' src={post.img} alt="" />
             <h1 className="text-xl font-bold">{post.title}</h1>
             <Link to={`/post/${post.id}`}><button className='self-start text-theme_dark border-b-[2px] border-theme_dark'>Read more</button></Link>
             
@@ -33,7 +33,7 @@ const Menu = ({id,cat=''}) => {
   return (
     <div>
         <h1 className="text-xl font-bold">Other posts you may like</h1>
-        <div className="posts flex gap-5 flex-col mt-5">
+        <div className="posts flex gap-5 items-stretch flex-col mt-5">
             {posts.filter(post=>{
                 return post.id!==id
             }).map(post=>{
