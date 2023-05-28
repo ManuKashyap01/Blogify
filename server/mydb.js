1ce677e57f5621252104e9a7846ff8b5da8cab4f
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-05-28 18:53:38 +0530

    mysql2 mydb.js port added

--- server/mydb.js
+++ server/mydb.js
@@ -3,10 +3,11 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 // used mysql2 instead of mysql as there was some connection error related to password
-console.log(process.env.USERNAME)
+// console.log(process.env.USERNAME)
 export const mydb=mysql.createConnection({
     host:process.env.HOST,
     user:process.env.USERNAME,
     password:process.env.PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.DATABASE,
+    port:process.env.PORT
 })
