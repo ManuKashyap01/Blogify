4c8f9129a2a59c09e83c16ccf8351e6efc4a3102
Manu kashyap <76866512+ManuKashyap01@users.noreply.github.com>
2025-08-11 22:44:16 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
       <div className="items-center bg-theme_dark/[0.8] flex justify-between py-5 sm:px-10 px-4">
         <div className="logo text-white font-bold sm:max-w-[100px] max-w-[75px]">
-          <Link to='/'>Manu <span className='text-black'>K.</span></Link>
+          <Link to='/'>Abhinav <span className='text-black'>V.</span></Link>
         </div>
         <p className="text-theme_light text-sm">Made with ❤ and React.js</p>
       </div>
